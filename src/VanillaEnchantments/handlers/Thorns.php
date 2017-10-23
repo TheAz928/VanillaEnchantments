c8032984daacfd62a9852532475552c96cf61e04
Az928 <TheAz928@users.noreply.github.com>
2017-10-23 10:11:00 +0600

    Fix thorns random chance

--- src/VanillaEnchantments/handlers/Thorns.php
+++ src/VanillaEnchantments/handlers/Thorns.php
@@ -24,25 +24,25 @@ class Thorns extends VanillaEnchant implements Listener{
 			 foreach($player->getInventory()->getArmorContents() as $armor){
 		       $level = $this->getEnchantmentLevel($armor, Enchantment::THORNS);
 		        if(in_array($armor->getId(), [298, 302, 306, 310, 314])){
-			       if(rand(1, 100) <= 15 + $level){
+			       if(rand(1, 100) <= 15 * $level){
 				      $this->addHelmetDurability($player, -3);
 				      $damager->attack(new EntityDamageByEntityEvent($player, $damager, $event::CAUSE_ENTITY_ATTACK, rand(1, 5)));
 				    }
 			     }
 			     if(in_array($armor->getId(), [299, 303, 307, 311, 315])){
-				    if(rand(1, 100) <= 15 + $level){
+				    if(rand(1, 100) <= 15 * $level){
 				      $this->addChestplateDurability($player, -3);
 				      $damager->attack(new EntityDamageByEntityEvent($player, $damager, $event::CAUSE_ENTITY_ATTACK, rand(1, 5)));
 				    }
 				  }
 				  if(in_array($armor->getId(), [300, 304, 308, 312, 316])){
-					 if(rand(1, 100) <= 15 + $level){
+					 if(rand(1, 100) <= 15 * $level){
 				      $this->addLeggingsDurability($player, -3);
 				      $damager->attack(new EntityDamageByEntityEvent($player, $damager, $event::CAUSE_ENTITY_ATTACK, rand(1, 5)));
 				    }
 				  }
 				  if(in_array($armor->getId(), [301, 305, 309, 313, 317])){
-					 if(rand(1, 100) <= 15 + $level){
+					 if(rand(1, 100) <= 15 * $level){
 				      $this->addBootsDurability($player, -3);
 				      $damager->attack(new EntityDamageByEntityEvent($player, $damager, $event::CAUSE_ENTITY_ATTACK, rand(1, 5)));
 				    }
