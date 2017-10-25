ac3ab16917a1087a6c52c35d5a3f4e90a3a10414
Az928 <TheAz928@users.noreply.github.com>
2017-10-25 18:56:23 +0600

    Silk touch improvement [1/∞]

--- src/VanillaEnchantments/handlers/SilkTouch.php
+++ src/VanillaEnchantments/handlers/SilkTouch.php
@@ -18,7 +18,15 @@ class SilkTouch extends VanillaEnchant implements Listener{
 	    $item = $event->getItem();
 	    $block = $event->getBlock();
 	    if($item->hasEnchantment(Enchantment::SILK_TOUCH) && $block->getId() !== 52){
-		   $event->setDrops([Item::get($block->getId(), $block->getDamage(), 1)]);
+		   $drops = [];
+		   foreach($event->getDrops() as $drop){
+		      if($drop->getId() == $block->getId() and $drop->getDamage() == $block->getDamage()){
+		        $drops[] = $drop;
+		      }else{
+		       $drop[] = Item::get($block->getId(), $item->getDamage(), 1);
+		      }
+		   }
+		   $event->setDrops($drops);
 		}
 	}
 }
