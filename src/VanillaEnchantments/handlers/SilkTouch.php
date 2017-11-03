0c80e3369c140f577d1095c6357a87541cbceba5
Az928 <TheAz928@users.noreply.github.com>
2017-11-03 22:25:36 +0600

    Update SilkTouch.php

--- src/VanillaEnchantments/handlers/SilkTouch.php
+++ src/VanillaEnchantments/handlers/SilkTouch.php
@@ -18,13 +18,13 @@ class SilkTouch extends VanillaEnchant implements Listener{
 	public function onBreak(BlockBreakEvent $event): void{
 	    $item = $event->getItem();
 	    $block = $event->getBlock();
-	    if($item->hasEnchantment(Enchantment::SILK_TOUCH) && $block->getId() !== 52){
+	    if($item->hasEnchantment(Enchantment::SILK_TOUCH) and $block->getId() !== 52){
 		   $drops = [];
 		   foreach($event->getDrops() as $drop){
 		      if($drop->getId() == $block->getId() and $drop->getDamage() == $block->getDamage()){
 		        $drops[] = $drop;
 		      }else{
-		       $drop[] = Item::get($block->getId(), $item->getDamage(), 1);
+		       $drop[] = Item::get($block->getId(), $block->getDamage(), 1);
 		      }
 		   }
 		   $event->setDrops($drops);
