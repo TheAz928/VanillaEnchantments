c0e51fd5d108e5687a5c4b011d6081736f2f6e54
Haxley <kdavis@plooms.com>
2018-01-29 00:26:34 -0700

    Add files via upload

--- src/VanillaEnchantments/handlers/Infinity.php
+++ src/VanillaEnchantments/handlers/Infinity.php
@@ -9,29 +9,50 @@ use pocketmine\item\enchantment\Enchantment;
 
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityShootBowEvent;
+use pocketmine\event\inventory\InventoryPickupArrowEvent;
 
 use VanillaEnchantments\Core;
 
 class Infinity extends VanillaEnchant implements Listener{
-	
+
 	public function __construct(Core $core){
 	    $core->getServer()->getPluginManager()->registerEvents($this, $core);
 	}
-	
+
+	/**
+	 * @param InventoryPickupArrowEvent $event
+	 */
+
+	public function cancelPickupArrow(InventoryPickupArrowEvent $event): void{
+			 $arrow = $event->getArrow();
+
+			 if($arrow->getNameTag() == "infinity") {
+				 $event->setCancelled();
+			 }
+
+	}
+
 	/**
 	 * @param EntityShootBowEvent $event
 	 */
-	
+
 	public function onShoot(EntityShootBowEvent $event): void{
-	      $player = $event->getEntity();
-	      $bow = $event->getBow();
-	      if($event->isCancelled()){
-		      return;
-		   }
+		 	$player = $event->getEntity();
+		 	$bow = $event->getBow();
+		 	$arrow = $event->getProjectile();
+
+		 	if($event->isCancelled()){
+		  	 return;
+	   	}
+
 	   	if($bow->hasEnchantment(Enchantment::INFINITY)){
+				 $arrow->setNameTag("infinity");
+
 			   if($player instanceof Player and $player->isSurvival()){
-			      $player->getInventory()->addItem(Item::get(Item::ARROW, 0, 1));
-		   	}
-	     }
+			       $player->getInventory()->addItem(Item::get(Item::ARROW, 0, 1));
+
+
+		   	 }
+	    }
 	 }
 }
