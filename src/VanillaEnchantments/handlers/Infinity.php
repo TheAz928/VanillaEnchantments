334dc9a82b9b254ff2cc0d2b9fe445934318dfd4
Az928 <TheAz928@users.noreply.github.com>
2018-01-29 13:58:05 +0600

    Fix infinity duplication

--- src/VanillaEnchantments/handlers/Infinity.php
+++ src/VanillaEnchantments/handlers/Infinity.php
@@ -9,6 +9,7 @@ use pocketmine\item\enchantment\Enchantment;
 
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityShootBowEvent;
+use pocketmine\event\entity\ProjectileHitEvent;
 
 use VanillaEnchantments\Core;
 
@@ -22,16 +23,29 @@ class Infinity extends VanillaEnchant implements Listener{
 	 * @param EntityShootBowEvent $event
 	 */
 	
-	public function onShoot(EntityShootBowEvent $event): void{
+	public function onShoot(EntityShootBowEvent $event) : void{
 	      $player = $event->getEntity();
 	      $bow = $event->getBow();
+	      $projectile = $event->getProjectile();
 	      if($event->isCancelled()){
 		      return;
 		   }
 	   	if($bow->hasEnchantment(Enchantment::INFINITY)){
 			   if($player instanceof Player and $player->isSurvival()){
+				   $projectile->namedtag->setShort("infinity", 1);
 			      $player->getInventory()->addItem(Item::get(Item::ARROW, 0, 1));
 		   	}
 	     }
 	 }
+	
+	/**
+	 * @param ProjectileHitEvent $event
+	 */
+	
+	public function onHit(ProjectileHitEvent $event) : void{
+			$projectile = $event->getEntity();
+			if($projectile->namedtag->getShort("infinity", 0) !== 0){
+				$projectile->close();
+			}
+	}
 }
