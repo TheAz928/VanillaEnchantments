ffe13c9c6362cd405fc7346bbb437ff81af6b0b6
Az928 <TheAz928@users.noreply.github.com>
2018-05-28 19:50:55 +0600

    Fixes #53

--- src/vanilla/entity/ExperienceOrb.php
+++ src/vanilla/entity/ExperienceOrb.php
@@ -47,9 +47,9 @@ class ExperienceOrb extends PMXP{
 				$distance = $vector->length();
 				$oneMinusDistance = (1 - $distance) ** 2;
 				if($oneMinusDistance > 0){
-					$this->motionX += $vector->x / $distance * $oneMinusDistance * 0.2;
-					$this->motionY += $vector->y / $distance * $oneMinusDistance * 0.2;
-					$this->motionZ += $vector->z / $distance * $oneMinusDistance * 0.2;
+					$this->motion->x += $vector->x / $distance * $oneMinusDistance * 0.2;
+					$this->motion->y += $vector->y / $distance * $oneMinusDistance * 0.2;
+					$this->motion->z += $vector->z / $distance * $oneMinusDistance * 0.2;
 				}
 				if($currentTarget->canPickupXp() and $this->boundingBox->intersectsWith($currentTarget->getBoundingBox())){
 					$this->flagForDespawn();
